9d7717d71b0b6a1cbd4832e02924b7f2689df1bb
Francesco Pandolfi <francesco.pan@gmail.com>
2020-01-31 22:04:41 +0100

    Test

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ $(document).ready(function () {
         contentType: "application/json",
         dataType: 'json',
         method: "GET",
+        mode: 'no-cors',
         success: function (data, stato) {
           console.log(data);
         },
